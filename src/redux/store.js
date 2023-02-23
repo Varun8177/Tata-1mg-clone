bee5a88ef2b7d76186a719276e8fe6b131502eb7
raj8679 <devraj8679@gmail.com>
2023-02-23 21:32:37 +0530

    added auth folder

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,8 @@
 import { applyMiddleware, combineReducers, legacy_createStore, compose } from "redux"
 import thunk from 'redux-thunk'
+import {reducer as authReducer} from "./auth/reducer"
 
-const rootReducer = combineReducers({
+const rootReducer = combineReducers({authReducer,
 })
 
 export const store = legacy_createStore(rootReducer, compose(applyMiddleware(thunk)))
