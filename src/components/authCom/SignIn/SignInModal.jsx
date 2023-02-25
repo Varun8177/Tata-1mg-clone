4b1e33acfc2779b9c06cd50d88acbc8a8ce4c84d
raj8679 <devraj8679@gmail.com>
2023-02-25 22:32:24 +0530

    modified cart navbar

---
+++ src/components/authCom/SignIn/SignInModal.jsx
@@ -0,0 +1,37 @@
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    Button,
+    useDisclosure,
+  } from '@chakra-ui/react'
+
+import SignInCard from './SignInCard'
+
+  export default function SignInModal() {
+   
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    
+    return (
+      <>
+        <Button  onClick={onOpen}>Sign-In</Button>
+  
+        <Modal isOpen={isOpen} onClose={onClose} size="4xl" >
+          <ModalOverlay />
+          <ModalContent>
+           
+            <ModalCloseButton />
+            <ModalBody>
+              <SignInCard />
+            </ModalBody>
+  
+           
+          </ModalContent>
+        </Modal>
+      </>
+    )
+  }
