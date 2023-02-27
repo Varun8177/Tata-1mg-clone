adc419f584b76109617fda0145add7167d6df863
meenakshiar <meenakshiarora2707@gmail.com>
2023-02-27 22:57:41 +0530

    updated skeleten.js and search functionality

--- src/components/navbar/MainNavbar/NavMiddleSection.jsx
+++ src/components/navbar/MainNavbar/NavMiddleSection.jsx
@@ -8,12 +8,30 @@ import {
   InputRightElement,
   Text,
 } from "@chakra-ui/react";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { AiFillThunderbolt } from "react-icons/ai";
 import { GiThunderBlade } from "react-icons/gi";
 import { ImLocation } from "react-icons/im";
 
 const NavMiddleSection = () => {
+
+  const [onChangeValue, setOnChangeValue] = useState('');
+  const [search, setSearch] = useState([]);
+
+  const searchMovies = async (query) => {
+    const data = await fetch(`https://dead-earrings-tick.cyclic.app/products`);
+    const res = await data.json();
+    setSearch(res)
+  }
+  useEffect(() => {
+    if (onChangeValue === '') { return }
+    const id = setTimeout(() => {
+      searchMovies(onChangeValue)
+    }, 1000)
+    return () => clearTimeout(id)
+  }, [onChangeValue]);
+  console.log(search)
+
   return (
     <Flex justifyContent={"space-around"} alignItems={"center"}>
       <Flex
@@ -34,6 +52,7 @@ const NavMiddleSection = () => {
           placeholder="Search for medicines and health products"
           size="sm"
           w={"100%"}
+          onChange={(e) => setOnChangeValue(e.target.value)}
         />
         {/* <InputRightElement>
           <Search2Icon />
